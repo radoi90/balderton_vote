a6645198f65797d38738ff1eed7e4df4bb5282e0
Emil Culic <eculic17@gmail.com>
2015-12-16 19:31:58 +0000

    Adds sample code

---
+++ cloud/app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var _ = require('underscore');
+
+
+// Controller code in separate files.
+var adminController = require('cloud/controllers/admin.js');
+
+
+// Required for initializing Express app in Cloud Code.
+var app = express();
+
+// Global app configuration section
+app.set('views', 'cloud/views');
+app.set('view engine', 'ejs');
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+
+app.locals._ = _;
+
+/**** ==== ROUTING ==== ****/
+
+// Show all posts on homepage
+app.get('/', adminController.index);
+
+
+// Route for admin pages
+app.get('/admin', adminController.index);
+
+// Required for initializing Express app in Cloud Code.
+app.listen();
