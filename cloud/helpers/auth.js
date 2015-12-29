9e2b61ad5a4cb1521734ccfeca2ef93f3df7ee3e
Emil Culic <eculic17@gmail.com>
2015-12-29 03:22:16 +0000

    Changes auth, loadUse flow

--- cloud/helpers/auth.js
+++ cloud/helpers/auth.js
@@ -1,18 +1,18 @@
 var _ = require('underscore');
 
 exports.authenticate = function(req, res, next) {
-	var noAuthRequired = _.contains(['/login', '/signup'], req.path);
+	var noAuthRequired = _.contains(noAuthPaths, req.path);
 	res.locals.current_user = undefined;
 	
-	if (noAuthRequired) { 
-		return next();
-	} else if (Parse.User.current()) { 
+	if (Parse.User.current()) { 
 		return Parse.User.current().fetch().then(function(user) {
 			res.locals.current_user = user;
 
 			return next();
 		});
-	}
+	} else if (noAuthRequired) { 
+		return next();
+	} 
 
 	res.redirect('/login?redir=' + req.path);
 };
@@ -32,3 +32,5 @@ exports.partnerOnly = function(req, res, next) {
 
 	res.status(404).send('Not Found');
 }
+
+var noAuthPaths = ['/login', '/signup'];
